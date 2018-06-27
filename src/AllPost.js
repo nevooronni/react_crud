ae754a12158de8f50b557e6752083daf9e8fb886
nevooronni <nevooronni@gmail.com>
2018-06-27 16:25:12 +0300

    created post component

--- src/AllPost.js
+++ src/AllPost.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Post from './Post';
 
 class AllPost extends Component {
     render() {
         return (
             <div>
                 <h1>All Posts</h1>
-                {console.log(this.props.posts)}
+                {this.props.posts.map((post) => <Post key={post.id} post={post} />)}
             </div>
         );
     }
