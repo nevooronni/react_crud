5abb64d756a8f69392cf0e97371e29136297bb14
nevooronni <nevooronni@gmail.com>
2018-06-28 11:34:40 +0300

    style post and all post component

--- src/PostForm.js
+++ src/PostForm.js
@@ -29,9 +29,9 @@ class PostForm extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Create Post</h1>
-                <form onSubmit={this.handleSubmit}>
+            <div className="post-container">
+                <h1 className="post_heading">Create Post</h1>
+                <form className="form" onSubmit={this.handleSubmit}>
                     <input required type="text" ref={(input)=>this.getTitle = input} placeholder="Enter Post Title" /><br /><br />
                     <textarea required rows="5" ref={(input)=>this.getMesssage = input} cols="28" placeholder="Enter Post" /><br /><br />
                     <button>Post</button>
